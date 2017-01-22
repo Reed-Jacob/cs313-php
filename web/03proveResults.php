c7cc8d210d23742aafb86cfb6493d72dcaebd8a1
Jacob Reed <jacobreedbyui@gmail.com>
2017-01-22 14:58:46 -0700

    Fixed (stupid) typo

--- web/03proveResults.php
+++ web/03proveResults.php
@@ -30,7 +30,7 @@ $no4 = $array[7];
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Prove 032</title>
+		<title>Prove 03</title>
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
